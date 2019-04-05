28aab60e3a0f92e081c1819de7db3223c8d65130
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-04-05 15:56:25 +0700

    Use  dependency to parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use structopt::StructOpt;
+
+/// search for a pattern in a file and display the lines that contains it
+#[derive(StructOpt)]
+struct CLI {
+    /// the pattern to look for
+    pattern: String,
+    // the path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
     println!("Hello, world!");
 }
