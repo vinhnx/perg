bcadb2dd1d1fe8efa6a0a59cb0ac38d6000cc7bd
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-04-18 22:57:10 +0700

    Note on test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use std::fmt;
 use structopt::StructOpt;
 
-/// search for a pattern in a file and display the lines that contains it
 #[derive(StructOpt)]
 #[derive(Debug)]
 struct CLI {
-    /// the pattern to look for
     pattern: String,
-    // the path to the file to read
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
@@ -38,6 +35,6 @@ fn main() {
 #[test]
 fn test_find_match() {
     let mut result = Vec::new();
-    find_matches("hello world\nbye word", "hello", &mut result);
-    assert_eq!(result, b"hello world\n");
+    find_matches("hello world\nbye word", "hello", &mut result); // we give empty vector as "writer" in our test
+    assert_eq!(result, b"hello world\n"); // b: is byte string literal
 }
