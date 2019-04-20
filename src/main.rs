4074a4cfe6c512563a4e85f62d147ad018252a65
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-04-20 22:27:46 +0700

    Add TODOs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,15 @@ use std::fmt;
 use std::fs::File;
 use std::io::{BufReader, Read};
 use structopt::StructOpt;
+use std::env;
+
+/*
+    TODO:
+    + case insensitive (-i)
+    + highlight matches
+    + mimic most of `grep` features, for education purposes
+        > https://www.digitalocean.com/community/tutorials/using-grep-regular-expressions-to-search-for-text-patterns-in-linux#regular-expressions
+*/
 
 #[derive(StructOpt, Debug)]
 struct CLI {
@@ -25,18 +34,21 @@ impl fmt::Display for CLI {
 }
 
 fn main() {
+    let vargs: Vec<String> = env::args().collect();
+    println!("{:?}", vargs);
+
     // parse CLI arguments
-    let args = CLI::from_args();
+    // let args = CLI::from_args();
     let file = File::open(&args.path).expect("could not read file");
 
-    // read content of file and appending to data
-    let mut data = String::new();
-    let mut reader = BufReader::new(file);
-    reader
-        .read_to_string(&mut data)
-        .expect("unable to read string");
+    // // read content of file and appending to data
+    // let mut data = String::new();
+    // let mut reader = BufReader::new(file);
+    // reader
+    //     .read_to_string(&mut data)
+    //     .expect("unable to read string");
 
-    // convert pattern into regular expression
-    let re = Regex::new(&args.pattern).unwrap();
-    perg::search(&data, &re, &mut std::io::stdout());
+    // // convert pattern into regular expression
+    // let re = Regex::new(&args.pattern).unwrap();
+    // perg::search(&data, &re, &mut std::io::stdout());
 }
