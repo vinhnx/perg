aa48f6640df5a876b418211f76212700696bff93
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-04-21 00:24:30 +0700

    Add case-insensitve search flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
 extern crate regex;
+use regex::Regex;
+
+// TODO: unify these with borrowing `to_lowercase()`
 
 #[allow(unused_must_use)]
-pub fn search(content: &str, pattern: &regex::Regex, mut writer: impl std::io::Write) {
+pub fn search(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    let regex = Regex::new(&pattern).unwrap();
     for line in content.lines() {
-        if pattern.is_match(line) {
+        if regex.is_match(line) {
+            writeln!(writer, "{}", line);
+        }
+    }
+}
+
+#[allow(unused_must_use)]
+pub fn search_case_insensitive(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    let regex = Regex::new(&pattern.to_lowercase()).unwrap();
+    for line in content.lines() {
+        if regex.is_match(line) {
             writeln!(writer, "{}", line);
         }
     }
